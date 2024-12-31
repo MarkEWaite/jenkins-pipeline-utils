fb962c98d8eacc4e70a3b209db259e5c8a4294d8
Mark Waite <mark.earl.waite@gmail.com>
2024-12-31 06:56:18 -0700

    Run Ant with Java 21, not Java 8
    Java 8 is not supported by the Jenkins project to run Jenkins controllers
    or Jenkins agents and Java 8 is not available on s390x.
    
    Also fixes a bug in the getSHA1 method when run on Windows

--- pipelineSteps.groovy
+++ pipelineSteps.groovy
@@ -18,7 +18,7 @@ def dumpEnvVars() {
 /* Run ant from tool "ant-latest" */
 void ant(def args) {
   /* Get jdk tool. */
-  String jdktool = tool name: "jdk8", type: 'hudson.model.JDK'
+  String jdktool = tool name: 'jdk21', type: 'hudson.model.JDK'
 
   /* Get the ant tool. */
   def antHome = tool name: 'ant-latest', type: 'hudson.tasks.Ant$AntInstallation'
@@ -40,18 +40,17 @@ void ant(def args) {
 
 def getSHA1(def commit) {
   if (isUnix()) {
-    // Should use JGit that is already included in the git plugin
     sha1 = sh(script: "git rev-parse ${commit}", returnStdout: true)
   } else {
     // Windows treats caret as special character, must escape it
     if (commit.contains("^")) {
       commit = commit.replace("^", "^^")
     }
-    sha1 = bat(script: "git rev-parse ${commit}", returnStdout: true)
+    // Windows returns command line before sha1 unless we ECHO OFF prior
+    sha1 = bat(script: "@ECHO OFF && git rev-parse ${commit}", returnStdout: true)
   }
   // Remove white space
   sha1 = sha1.replaceAll("\\s", "")
-  // Windows returns command line before sha1 - discard all but sha1
   if (sha1.length() > 40) {
     sha1 = sha1.substring(sha1.length() - 40)
   }
